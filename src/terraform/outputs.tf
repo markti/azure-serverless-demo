8655260170fdbe2cf4f24a4113be3afb48925652
markti <markti@outlook.com>
2023-11-08 13:25:00 -0500

    fix output

--- src/terraform/outputs.tf
+++ src/terraform/outputs.tf
@@ -2,7 +2,7 @@ output "frontend_storage_account_name" {
   value = azurerm_storage_account.frontend.name
 }
 output "function_name" {
-  value = azurerm_linux_function_app.github_crawler.name
+  value = azurerm_linux_function_app.main.name
 }
 output "resource_group_name" {
   value = azurerm_resource_group.main.name

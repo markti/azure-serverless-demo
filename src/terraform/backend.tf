6c9b5e6bef1c2bf2bbc334853a1df2d704401e3f
markti <markti@outlook.com>
2023-11-08 15:24:02 -0500

    add scm do

--- src/terraform/backend.tf
+++ src/terraform/backend.tf
@@ -39,8 +39,9 @@ resource "azurerm_linux_function_app" "main" {
   }
 
   app_settings = {
-    "WEBSITE_RUN_FROM_PACKAGE"  = 1
-    "STORAGE_CONNECTION_STRING" = azurerm_storage_account.function.primary_connection_string
-    "QUEUE_CONNECTION_STRING"   = azurerm_storage_account.function.primary_connection_string
+    "SCM_DO_BUILD_DURING_DEPLOYMENT" = true
+    "WEBSITE_RUN_FROM_PACKAGE"       = 1
+    "STORAGE_CONNECTION_STRING"      = azurerm_storage_account.function.primary_connection_string
+    "QUEUE_CONNECTION_STRING"        = azurerm_storage_account.function.primary_connection_string
   }
 }

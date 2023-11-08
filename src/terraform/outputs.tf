362f7387f7555b70ac809097608fc78f0a962b85
markti <markti@outlook.com>
2023-11-08 13:21:04 -0500

    add outputs for fn

--- src/terraform/outputs.tf
+++ src/terraform/outputs.tf
@@ -1,3 +1,9 @@
 output "frontend_storage_account_name" {
   value = azurerm_storage_account.frontend.name
 }
+output "function_name" {
+  value = azurerm_linux_function_app.github_crawler.name
+}
+output "resource_group_name" {
+  value = azurerm_resource_group.main.name
+}

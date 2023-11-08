a56202079eacbe4c270bf3b50b260fd6c9d6ab56
markti <markti@outlook.com>
2023-11-08 12:55:47 -0500

    set index.html

--- src/terraform/frontend.tf
+++ src/terraform/frontend.tf
@@ -7,7 +7,7 @@ resource "azurerm_storage_account" "frontend" {
   account_replication_type = "LRS"
 
   static_website {
-    index_document     = "wwwroot/index.html"
+    index_document     = "index.html"
     error_404_document = "404.html"
   }
 }

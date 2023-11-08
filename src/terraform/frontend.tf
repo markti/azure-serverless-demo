673017f69fbfb41640916cb879408c6c5e54c294
markti <markti@outlook.com>
2023-11-08 10:51:28 -0500

    upload staging

--- src/terraform/frontend.tf
+++ src/terraform/frontend.tf
@@ -11,9 +11,3 @@ resource "azurerm_storage_account" "frontend" {
     error_404_document = "404.html"
   }
 }
-
-resource "azurerm_static_site" "main" {
-  name                = "stapp-${var.application_name}-${var.environment_name}-${random_string.main.result}"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-}

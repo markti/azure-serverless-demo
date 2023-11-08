9a3cd3692f7c7972680d5950e0b7c69e06a297f9
markti <markti@outlook.com>
2023-11-08 07:12:16 -0500

    default_host_name

--- src/terraform/frontend.tf
+++ src/terraform/frontend.tf
@@ -7,7 +7,7 @@ resource "azurerm_storage_account" "frontend" {
   account_replication_type = "LRS"
 }
 
-resource "azurerm_static_site" "frontend" {
+resource "azurerm_static_site" "main" {
   name                = "stapp-${var.application_name}-${var.environment_name}-${random_string.main.result}"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
--- src/terraform/outputs.tf
+++ src/terraform/outputs.tf
@@ -1,3 +1,3 @@
 output "primary_web_endpoint" {
-  value = azurerm_storage_account_static_website.example.primary_web_endpoint
+  value = azurerm_static_site.main.default_host_name
 }

3b7c4dcef666b432c7bc9c14efebb28e731d78e3
markti <markti@outlook.com>
2023-11-10 11:46:24 -0500

    30d

--- src/terraform/windows-consumable/monitor.tf
+++ src/terraform/windows-consumable/monitor.tf
@@ -3,5 +3,5 @@ resource "azurerm_log_analytics_workspace" "main" {
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   sku                 = "PerGB2018"
-  retention_in_days   = 7
+  retention_in_days   = 30
 }
